7a015945a0c6dc377cc688760017ec10b2da16a6
AjiePancaZero <ajiepanca0@gmail.com>
2022-09-24 20:19:03 +0700

    berhasil menampilkan semua data dari table mahasiswa

--- controller.js
+++ controller.js
@@ -2,7 +2,19 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const con = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi RestApi Berjalan",res)
+};
+
+//menampilkan semua data mahasisawa
+exports.tampilsemuadatamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    });
 };
